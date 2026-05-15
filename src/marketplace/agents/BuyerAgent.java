cf1166fb0e00cecbf90cb5869454f404a0c93c68
Pelayo <p.aguinaco@alumnos.upm.es>
2026-05-15 12:18:47 +0200

    Prueba de push

--- src/marketplace/agents/BuyerAgent.java
+++ src/marketplace/agents/BuyerAgent.java
@@ -57,7 +57,7 @@ public class BuyerAgent extends Agent {
 	@Override
 	protected void takeDown() {
 		System.out.println(" Comprador [ " + getLocalName()+  "] abandonando el mercado.");
-		System.out.println("----------------------------------------");
+		System.out.println("---------------------------------------");
 
 	}
 	
